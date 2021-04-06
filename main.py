222a0c6913c6c2549ff189e9f24ebb4a3b22387e
ORMmaster <82022877+ORMmaster@users.noreply.github.com>
2021-04-06 13:37:55 +0530

    main1.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#for defining n inputs from the user
+print('enter 0 to stop.\n')
+
+def enter(x):
+	while x:
+		x=input()
+x=input()
+enter(x)
+#------------------ done ---------------------------
+
